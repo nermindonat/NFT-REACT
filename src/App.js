78d4f074943b33886f8658ddfafe16ccf3ab977b
Nermin Donat <70170606+nermindonat@users.noreply.github.com>
2022-08-12 13:08:01 +0300

    Some bugs fixed.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import "./App.css";
 import Home from "./pages/home/Home";
 import { Web3ReactProvider } from "@web3-react/core";
-import {Web3} from "web3";
-import Collection from "./components/collections/Collection";
+//import {} from "web3";
 
-
-const getLibrary = provider => {
-  return new Web3(provider)
-}
 
 const App = () => {
   return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <div className="App">
-        <Home />
-        <br></br>
-        <Collection />
-      </div>
-    </Web3ReactProvider>
+    <Home />
   );
 };
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,37 +1,57 @@
-import React from 'react'
-import "./home.css"
-import { useWeb3React } from '@web3-react/core'
-import { injected } from '../../components/connectors/injected';
+import React from "react";
+import "./home.css";
+import { useWeb3React } from "@web3-react/core";
+import { injected } from "../../components/connectors/injected";
+import { Web3ReactProvider } from "@web3-react/core";
+import Collection from "../collections/Collection";
 
 const Home = () => {
-  const {active, acount, activate, deactivate} = useWeb3React();
-  
+  const { active, acount, activate, deactivate } = useWeb3React();
+
+  const getLibrary = (provider) => {
+    return new Web3ReactProvider(provider);
+  };
+
   // click to connect
-  const connect = async() => {
+  const connect = async () => {
     try {
-      await activate(injected)
+      console.log(injected)
+      let response = await activate(injected);
+      if (response) {
+        console.log("response", response);
+      }
     } catch (error) {
       console.log(error);
     }
-  }
+  };
+
+ 
 
   return (
-    <div className='metamask'>
-      <div className="connect-metamask">
-        <img className='logo' src="/assets/images/logo.png" alt="" />
-        <h2>Connet to MetaMask</h2>
-      </div>
-      <button className='home-button' onClick={connect} >
-        <img className='logo' src="/assets/images/polygon_logo.png" alt="" />
-        Plygon Network
-      </button>
-      <button className='home-button' onClick={connect} >
-        <img className='logo' src="/assets/images/bsc_logo.png" alt="" />
-        BSC Network
-      </button>
-      
-    </div>
-  )
-}
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <div className="App">
+        <div className="metamask">
+          <div className="connect-metamask">
+            <img className="logo" src="/assets/images/logo.png" alt="" />
+            <span>Connet to MetaMask</span>
+          </div>
+          <button className="home-button" onClick={connect}>
+            <img
+              className="logo"
+              src="/assets/images/polygon_logo.png"
+              alt=""
+            />
+            Plygon Network
+          </button>
 
-export default Home
+          <button className="home-button" onClick={connect}>
+            <img className="logo" src="/assets/images/bsc_logo.png" alt="" />
+            BSC Network
+          </button>
+        </div>
+      </div>
+    </Web3ReactProvider>
+  );
+};
+
+export default Home;
